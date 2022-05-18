24781e8c9d3f2cc44427f4cc6672c5e8a798e7a7
FanYJ <sd19fyj@qq.com>
2022-05-19 00:43:26 +0800

    ...

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+hahah = "kjalsdkjfldsjf"
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
